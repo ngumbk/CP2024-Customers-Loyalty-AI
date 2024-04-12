68205854099a248eeda8b19a0c5e6f658d4f4909
ngumbk <chulkov.nikita01@gmail.com>
2024-04-13 00:26:26 +0300

    Создан базовый django-проект

---
+++ cp2024_customers_loyalty/cp2024_customers_loyalty/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cp2024_customers_loyalty project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp2024_customers_loyalty.settings')
+
+application = get_asgi_application()
---
+++ cp2024_customers_loyalty/customers_loyalty_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomersLoyaltyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'customers_loyalty_app'
